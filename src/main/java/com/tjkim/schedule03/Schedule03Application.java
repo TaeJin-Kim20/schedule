c62067d76f3f73822a529ad6171e932bfb8dc22a
Taejin Kim <kimtaejin20@gmail.com>
2019-02-19 00:09:08 +0900

    Init commit

---
+++ src/main/java/com/tjkim/schedule03/Schedule03Application.java
@@ -0,0 +1,13 @@
+package com.tjkim.schedule03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schedule03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schedule03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjkim/schedule03/Schedule03ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tjkim.schedule03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Schedule03ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
